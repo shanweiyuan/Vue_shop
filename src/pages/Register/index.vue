e9e9008f87301feef1d201a6505a7c5668b685ca
stargazer <stargazer@swy.com>
2022-10-16 20:33:28 +0800

    init

---
+++ src/pages/Register/index.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  我是注册
+</div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,47 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+import Home from "@/pages/Home";
+import Search from "@/pages/Search";
+import Login from "@/pages/Login";
+import Register from "@/pages/Register";
+
+export default new VueRouter({
+    //配置路由
+    routes:[
+        {
+            path:"/home",
+            component:Home,
+            mate:{
+                show:true
+            }
+        },
+        {
+            path:"/search",
+            component:Search,
+            mate:{
+                show:true
+            }
+        },
+        {
+            path:"/login",
+            component:Login,
+            mate:{
+                show:false
+            }
+        },
+        {
+            path:"/register",
+            component:Register,
+            mate:{
+                show:false
+            }
+        },
+        //重定向，在项目跑起来的时候，访问/，立马让他定向到Home
+        {
+            path:'*',
+            redirect:"/Home"
+        }
+    ]
+})
